5f9cc573b5395d53b7407b31c2024d8b2c581a4a
dp7sv <dp7sv@virginia.edu>
2020-02-24 18:11:09 -0500

    added to java file

--- Java/src/BruteForce.java
+++ Java/src/BruteForce.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 
 public class BruteForce {
 
-	static long b1 = 0x0004010040100401l;
-	static long b2 = 0x00000000000003ffl;
-
 	public static void main(String[] args) throws FileNotFoundException {
 
 		String poemSRC = "./resources/poem1.txt";
@@ -17,61 +14,61 @@ public class BruteForce {
 		ArrayList<Long> words = parseWordsFromFile(poemSRC);
 
 		// print out each word for testing
-		words.forEach(i -> System.out.printf("%x\n", i));
+		//words.forEach(i -> System.out.printf("%x\n", i));
 
 		// scan in tiles from file
 		ArrayList<Long> tiles = parseTilesFromFile(tileSRC);
 
 		// print out each word for testing
-		tiles.forEach(i -> System.out.printf("%x\n", i));
+		//tiles.forEach(i -> System.out.printf("%x\n", i));
 
 		tile(tiles, words, "");
 
 	}
 
 	public static void tile(ArrayList<Long> t, ArrayList<Long> w, String p) {
-		long tile = t.remove(t.size()-1);
+		long tile = t.remove(t.size() - 1);
 		int iMax = maxLeftShift(tile);
 		int jMax = maxDownShift(tile);
-		for (int i = 0; i <= iMax; i++) {
-			for (int j = 0; j <= jMax; j++) {
-				System.out.println("here "+i+j);
+		for (int j = 0; j <= jMax; j++) {
+			for (int i = 0; i <= iMax; i++) {
 				long temp = (tile >> i) >> (10 * j);
 				ArrayList<Long> usedWords = new ArrayList<Long>();
-				for(int k = w.size()-1;k>=0;k--) {
+				for (int k = w.size() - 1; k >= 0; k--) {
 					long word = w.get(k);
-					if ((temp&word)!=0) {
-						if(!covers(temp,word)) {
-							
+					if ((temp & word) != 0) {
+						if (!covers(temp, word)) {
 							break;
-						}else {
+						} else {
 							w.remove(k);
 							usedWords.add(word);
-							temp^=word;
+							temp ^= word;
 						}
 					}
-					
+
 				}
-				if(temp == 0l) {
-					if(t.isEmpty() && w.isEmpty()) {
+				if (temp == 0l) {
+					if (t.isEmpty() && w.isEmpty()) {
 						System.out.println(p);
 						System.exit(0);
-					}else if(t.isEmpty()) {
+					} else if (t.isEmpty()) {
 						System.err.println("not enough tiles");
-						System.exit(1);
-					}else if(t.isEmpty()) {
+						//System.exit(1);
+					} else if (t.isEmpty()) {
 						System.err.println("too many tiles");
-						System.exit(1);
-					}else {
-						tile(t,w,p+"Shift tile "+ (12-t.size())+ " by (" + i + ", " + j+ ")\n");
+						//System.exit(1);
+					} else {
+						System.out.println("Placed tile " + (12 - t.size()) + " at (" + i + ", " + j + ")");
+						tile(t, w, p + "Shift tile " + (12 - t.size()) + " by (" + i + ", " + j + ")\n");
 					}
-					System.out.println("here "+p);
-				w.addAll(usedWords);
+
 				}
+				// System.out.println("here " + p);
+				w.addAll(usedWords);
 
 			}
 		}
-		
+
 	}
 
 	public static boolean covers(long t, long w) {
@@ -102,7 +99,7 @@ public class BruteForce {
 		in.close();
 		return words;
 	}
-	
+
 	public static ArrayList<Long> parseTilesFromFile(String src) throws FileNotFoundException {
 		ArrayList<Long> tiles = new ArrayList<Long>();
 		File t = new File(src);
@@ -113,24 +110,27 @@ public class BruteForce {
 		in.close();
 		return tiles;
 	}
-	
+
 	public static int maxLeftShift(long l) {
 		int ret = 9;
 		long mask1 = 0x0401004010040100l;
 		long mask2 = 0x0200802008020080l;
 		long mask3 = 0x0100401004010040l;
 		long mask4 = 0x0080200802008020l;
-		return (l&mask1)!=0?(l&mask2)!=0?(l&mask3)!=0?(l&mask4)!=0?ret-4:ret-3:ret-2:ret-1:ret;
+		return (l & mask1) != 0
+				? (l & mask2) != 0 ? (l & mask3) != 0 ? (l & mask4) != 0 ? ret - 4 : ret - 3 : ret - 2 : ret - 1
+				: ret;
 	}
-	
+
 	public static int maxDownShift(long l) {
 		int ret = 5;
 		long mask1 = 0x0003ff0000000000l;
 		long mask2 = 0x000000ffc0000000l;
 		long mask3 = 0x000000003ff00000l;
 		long mask4 = 0x00000000000ffc00l;
-		return (l&mask1)!=0?(l&mask2)!=0?(l&mask3)!=0?(l&mask4)!=0?ret-4:ret-3:ret-2:ret-1:ret;
+		return (l & mask1) != 0
+				? (l & mask2) != 0 ? (l & mask3) != 0 ? (l & mask4) != 0 ? ret - 4 : ret - 3 : ret - 2 : ret - 1
+				: ret;
 	}
-
 
 }
