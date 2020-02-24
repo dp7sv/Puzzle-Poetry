8e213c55d191aa7f0f897685782376c12fa87321
dp7sv <dp7sv@virginia.edu>
2020-02-24 10:13:56 -0500

    added to java program

--- Java/src/BruteForce.java
+++ Java/src/BruteForce.java
@@ -9,74 +9,128 @@ public class BruteForce {
 	static long b2 = 0x00000000000003ffl;
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		String poemSRC = "./resources/poem1.txt";
 		String tileSRC = "./resources/tiles1.txt";
+
+		// parsing file into set of words
+		ArrayList<Long> words = parseWordsFromFile(poemSRC);
+
+		// print out each word for testing
+		words.forEach(i -> System.out.printf("%x\n", i));
+
+		// scan in tiles from file
+		ArrayList<Long> tiles = parseTilesFromFile(tileSRC);
+
+		// print out each word for testing
+		tiles.forEach(i -> System.out.printf("%x\n", i));
+
+		tile(tiles, words, "");
+
+	}
+
+	public static void tile(ArrayList<Long> t, ArrayList<Long> w, String p) {
+		long tile = t.remove(t.size()-1);
+		int iMax = maxLeftShift(tile);
+		int jMax = maxDownShift(tile);
+		for (int i = 0; i <= iMax; i++) {
+			for (int j = 0; j <= jMax; j++) {
+				System.out.println("here "+i+j);
+				long temp = (tile >> i) >> (10 * j);
+				ArrayList<Long> usedWords = new ArrayList<Long>();
+				for(int k = w.size()-1;k>=0;k--) {
+					long word = w.get(k);
+					if ((temp&word)!=0) {
+						if(!covers(temp,word)) {
+							
+							break;
+						}else {
+							w.remove(k);
+							usedWords.add(word);
+							temp^=word;
+						}
+					}
+					
+				}
+				if(temp == 0l) {
+					if(t.isEmpty() && w.isEmpty()) {
+						System.out.println(p);
+						System.exit(0);
+					}else if(t.isEmpty()) {
+						System.err.println("not enough tiles");
+						System.exit(1);
+					}else if(t.isEmpty()) {
+						System.err.println("too many tiles");
+						System.exit(1);
+					}else {
+						tile(t,w,p+"Shift tile "+ (12-t.size())+ " by (" + i + ", " + j+ ")\n");
+					}
+					System.out.println("here "+p);
+				w.addAll(usedWords);
+				}
+
+			}
+		}
 		
-		//parsing file into set of words
+	}
+
+	public static boolean covers(long t, long w) {
+		return ((t ^ w) & w) == 0l ? true : false;
+	}
+
+	public static ArrayList<Long> parseWordsFromFile(String src) throws FileNotFoundException {
 		ArrayList<Long> words = new ArrayList<Long>();
-		File w = new File(poemSRC);
+		File w = new File(src);
 		Scanner in = new Scanner(w);
 		char curChar = 'A';
 		long temp = 0l;
-		for(int i = 59;i>=0;i--) {
+		for (int i = 59; i >= 0; i--) {
 			char c = in.next().charAt(0);
-			if(c!=curChar) {
+			if (c != curChar) {
 				curChar++;
-				if(c!=curChar) {
-					System.err.println("poem not formatted correctly at char "+(60 - i));
+				if (c != curChar) {
+					System.err.println("poem not formatted correctly at char " + (60 - i));
 					System.exit(1);
 				}
 				words.add(temp);
 				temp = 0l;
 			}
-			temp|=(1l<<i);
-			
+			temp |= (1l << i);
+
 		}
 		words.add(temp);
 		in.close();
-
-		//print out each word for testing
-		//words.forEach(i -> System.out.printf("%x\n", i));
-		
-		//scan in tiles from file
+		return words;
+	}
+	
+	public static ArrayList<Long> parseTilesFromFile(String src) throws FileNotFoundException {
 		ArrayList<Long> tiles = new ArrayList<Long>();
-		File t = new File(tileSRC);
-		in = new Scanner(t);
-		while(in.hasNext()) {
+		File t = new File(src);
+		Scanner in = new Scanner(t);
+		while (in.hasNext()) {
 			tiles.add(in.nextLong(16));
 		}
 		in.close();
-		
-		//print out each word for testing
-		tiles.forEach(i -> System.out.printf("%x\n", i));
-		
-	
-		tile(tiles,words);
-		
-
-	}
-
-	public static void tile(ArrayList<Long> t, ArrayList<Long> w) {
-		long tile = t.remove(t.size()-1);
-		
-		
+		return tiles;
 	}
 	
-	public static boolean find(long t, ArrayList<Long> w) {
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 6; j++) {
-				long temp = (t >> i) >> (10 * j);
-				
-				
-			}
-		}
-		return false;
-
+	public static int maxLeftShift(long l) {
+		int ret = 9;
+		long mask1 = 0x0401004010040100l;
+		long mask2 = 0x0200802008020080l;
+		long mask3 = 0x0100401004010040l;
+		long mask4 = 0x0080200802008020l;
+		return (l&mask1)!=0?(l&mask2)!=0?(l&mask3)!=0?(l&mask4)!=0?ret-4:ret-3:ret-2:ret-1:ret;
 	}
 	
-	public static boolean covers(long t, long w) {
-		return ((t^w)&w)==0l?true:false;
+	public static int maxDownShift(long l) {
+		int ret = 5;
+		long mask1 = 0x0003ff0000000000l;
+		long mask2 = 0x000000ffc0000000l;
+		long mask3 = 0x000000003ff00000l;
+		long mask4 = 0x00000000000ffc00l;
+		return (l&mask1)!=0?(l&mask2)!=0?(l&mask3)!=0?(l&mask4)!=0?ret-4:ret-3:ret-2:ret-1:ret;
 	}
+
 
 }
